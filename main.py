2ef03b7c1ee6264846d2b5c84952a853bb33d3fa
hliang21 <81581398+hliang21@users.noreply.github.com>
2021-03-29 10:07:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# RandomBoard Exercise
+import random
+
+
+def main():
+    arr = []
+    num_row = 10
+    num_col = 10
+
+    for i in range(num_row):
+        row = []
+        for j in range(num_col):
+            row.append(random.randint(0, 9))
+        arr.append(row)
+
+    print_2d_array(arr)
+    find_sum(arr)
+
+
+def print_2d_array(arr):
+    for x in arr:
+        for y in x:
+            print(y, end=' ')
+        print()
+
+
+def find_sum(arr):
+    sum_of_arr = 0
+    for x in arr:
+        for y in x:
+            sum_of_arr += y
+
+    print('The sum is ' + str(sum_of_arr))
+
+
+if __name__ == '__main__':
+    main()
